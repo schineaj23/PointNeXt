2b5a427cfb3484b63c38bed05d3afddecdcbf84c
anshgwash <anshgwash@vt.edu>
2026-02-16 12:06:52 -0500

    make build work on my machine

--- install.sh
+++ install.sh
@@ -7,48 +7,64 @@
 #source ~/.bashrc
 
 
-# The following 4 lines are only for slurm machines. uncomment if needed.  
-export TORCH_CUDA_ARCH_LIST="6.1;6.2;7.0;7.5;8.0"   # a100: 8.0; v100: 7.0; 2080ti: 7.5; titan xp: 6.1
-module purge
-module load cuda/11.1.1
-module load gcc/7.5.0
-
 # download openpoints
 # git submodule add git@github.com:guochengqian/openpoints.git
 git submodule update --init --recursive
-git submodule update --remote --merge # update to the latest version
+git submodule update --remote --merge 
 
-# install PyTorch
+# 1. Clean up
 conda deactivate
 conda env remove --name openpoints
-conda create -n openpoints -y python=3.7 numpy=1.20 numba
+conda clean --all -y
+pip cache purge
+
+# 2. Create Env
+conda create -n openpoints -y python=3.12 numpy numba
 conda activate openpoints
 
-# please always double check installation for pytorch and torch-scatter from the official documentation
-conda install -y pytorch=1.10.1 torchvision cudatoolkit=11.3 -c pytorch -c nvidia
-pip install torch-scatter -f https://data.pyg.org/whl/torch-1.10.1+cu113.html
+# 3. Install PyTorch + Toolkit + Compiler (ALL VER 12.6)
+# We use 12.6 because it is the most stable release currently available.
+conda install pytorch torchvision torchaudio pytorch-cuda=11.5 -c pytorch-nightly -c nvidia
+# ^^^ instead of this just run pip3 install torch torchvision --index-url https://download.pytorch.org/whl/cu130 --upgrade
+# afterwards and force the upgrade. else do some conda-forge fuckery i don't want to get into
 
-pip install -r requirements.txt
+# 4. CRITICAL: Override System CUDA
+# This forces the setup.py scripts to use the Conda compiler (12.6), not your system compiler (13.0)
+# export CUDA_HOME=$CONDA_PREFIX
+# export PATH=$CONDA_PREFIX/bin:$PATH
 
-# install cpp extensions, the pointnet++ library
+# Verify we are seeing the Conda compiler
+which nvcc
+# Output MUST be: .../envs/openpoints/bin/nvcc
+
+# 5. Clean & Build Dependencies
+# (Make sure you removed 'torch' and 'torch-scatter' from requirements.txt first!)
+pip --no-cache-dir install -r requirements.txt
+
+# 6. Install torch-scatter from source (to match local 12.6)
+# pip install --verbose --no-build-isolation git+https://github.com/rusty1s/pytorch_scatter.git
+
+# 7. Compile OpenPoints Extensions
+clean_build() { rm -rf build dist *.egg-info; }
+
 cd openpoints/cpp/pointnet2_batch
+clean_build
 python setup.py install
 cd ../
 
-# grid_subsampling library. necessary only if interested in S3DIS_sphere
 cd subsampling
+clean_build
 python setup.py build_ext --inplace
 cd ..
 
-
-# # point transformer library. Necessary only if interested in Point Transformer and Stratified Transformer
 cd pointops/
+clean_build
 python setup.py install
 cd ..
 
 # Blow are functions that optional. Necessary only if interested in reconstruction tasks such as completion
 cd chamfer_dist
-python setup.py install --user
+python setup.py install --user # actually run pip3 install -e . --no-build-isolation
 cd ../emd
-python setup.py install --user
+python setup.py install --user # actually run pip3 install -e . --no-build-isolation
 cd ../../../
